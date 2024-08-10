5a780b41e05637e946d34080540c246966949049
Keerthan S <Keerthans2003@gmail.com>
2024-08-10 12:35:54 +0530

    added analytics

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 export { firestore };
+
+isSupported().then((supported) => {
+  if (supported) {
+    const analytics = getAnalytics();
+  } else {
+    // Handle analytics not supported
+  }
+});
